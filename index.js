3b4aa5188f0ac5358981060d7202d88ba1fe98fc
John <shoemjoh@gmail.com>
2023-10-14 15:00:47 -0400

    arrays lab for phase 1

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    const otherCats = [name, ...cats];
+    return otherCats;
+}
+
+function removeLastCat() {
+    const shorterCats = cats.slice(0, cats.length - 1);
+    return shorterCats;
+}
+
+function removeFirstCat() {
+    const backCats = cats.slice(1, cats.length);
+    return backCats;
+}
